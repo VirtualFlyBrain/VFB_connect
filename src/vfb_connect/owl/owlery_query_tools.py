ca94b2e0b9f35ea836260347bc43aa3c51ccb563
Rob Court <rcourt@ed.ac.uk>
2020-05-10 11:48:56 +0100

    syntax fix

--- src/vfb_connect/owl/owlery_query_tools.py
+++ src/vfb_connect/owl/owlery_query_tools.py
@@ -51,7 +51,7 @@ class OWLeryConnect:
         r = requests.get(url=owl_endpoint, params=payload)
         print("Query URL: " + r.url)
         if r.status_code == 200:
-            print("Query results: " + r.json()[return_type].length)
+            print("Query results: " + str(len(r.json()[return_type])))
             return r.json()[return_type]
         else:
             warnings.warn(str(r.content))
