65613aa3c203b103e3b49b0ea8c80e29542029ce
Rob Court <rcourt@ed.ac.uk>
2024-08-14 01:49:07 +0100

    fixing tests

--- src/vfb_connect/neo/test/neo_tools_tests.py
+++ src/vfb_connect/neo/test/neo_tools_tests.py
@@ -1,15 +1,6 @@
-'''
-Created on Oct 24, 2016
-
-@author: davidos
-'''
 import unittest
 from ..neo4j_tools import Neo4jConnect
 from vfb_connect.neo.query_wrapper import QueryWrapper
-
-
-# TODO- pull over core nc tests from VFB_neo4j
-
 
 class NeoQueryWrapperTest(unittest.TestCase):
 
@@ -18,53 +9,70 @@ class NeoQueryWrapperTest(unittest.TestCase):
         self.qw = QueryWrapper()
 
     def test_get_term_info(self):
-        fu = self.qw.get_TermInfo(['FBbt_00003686', 'VFB_00010001', 'Ito2013'], summary=False)
-        self.assertTrue(
-            len(fu) == 3)
+        fu = self.qw.get_TermInfo(['FBbt_00003686', 'VFB_00010001', 'Ito2013'], summary=False, return_dataframe=False)
+        self.assertEqual(len(fu), 3)
 
     def test_get_type_term_info(self):
-        self.assertTrue(self.qw.get_type_TermInfo(short_forms=['FBbt_00003686'], summary=False))
-        self.assertTrue(self.qw.get_type_TermInfo(short_forms=['FBbt_00003686']))
+        result = self.qw.get_type_TermInfo(short_forms=['FBbt_00003686'], summary=False, return_dataframe=False)
+        self.assertIsInstance(result, list)
+        self.assertGreater(len(result), 0)
+
+        df = self.qw.get_type_TermInfo(short_forms=['FBbt_00003686'])
+        self.assertTrue(not df.empty)
 
     def test_get_DataSet_TermInfo(self):
-        self.assertTrue(
-            self.qw.get_DataSet_TermInfo(['Ito2013']))
+        df = self.qw.get_DataSet_TermInfo(['Ito2013'])
+        self.assertTrue(not df.empty)
 
     def test_get_anatomical_individual_TermInfo(self):
-        self.assertTrue(
-            self.qw.get_anatomical_individual_TermInfo(['VFB_00010001'], summary=False))
-        self.assertTrue(
-            self.qw.get_anatomical_individual_TermInfo(['VFB_00010001']))
+        result = self.qw.get_anatomical_individual_TermInfo(['VFB_00010001'], summary=False, return_dataframe=False)
+        self.assertIsInstance(result, list)
+        self.assertGreater(len(result), 0)
+
+        df = self.qw.get_anatomical_individual_TermInfo(['VFB_00010001'])
+        self.assertTrue(not df.empty)
 
     def test_get_terms_by_xref(self):
-        self.assertTrue(self.qw.get_terms_by_xref(['Trh-F-500041'], db='FlyCircuit'))
+        df = self.qw.get_terms_by_xref(['Trh-F-500041'], db='FlyCircuit')
+        self.assertTrue(not df.empty)
 
     def test_get_vfb_id_by_xref(self):
-        self.assertTrue(self.qw.xref_2_vfb_id(['Trh-F-500041'], db='FlyCircuit'))
-        self.assertTrue(self.qw.xref_2_vfb_id(db='FlyCircuit'))
+        result = self.qw.xref_2_vfb_id(['Trh-F-500041'], db='FlyCircuit')
+        self.assertIsInstance(result, dict)
+        self.assertGreater(len(result), 0)
+
+        result_all = self.qw.xref_2_vfb_id(db='FlyCircuit')
+        self.assertIsInstance(result_all, dict)
 
     def test_get_xref_by_vfbid(self):
-        self.assertTrue(self.qw.vfb_id_2_xrefs(['VFB_00014110'], db='FlyCircuit'))
+        result = self.qw.vfb_id_2_xrefs(['VFB_00014110'], db='FlyCircuit')
+        self.assertIsInstance(result, dict)
+        self.assertGreater(len(result), 0)
 
     def test_get_image_by_filename(self):
-        # super slow!
-#        self.assertTrue(
-#            self.vc.neo_query_wrapper.get_images_by_filename(
-#                ['JFRC2_MBON-b1-a.nrrd']))
-        self.assertTrue(
-            self.qw.get_images_by_filename(
-                ['JFRC2_MBON-b1-a.nrrd']))
-        self.assertTrue(
-            self.qw.get_images_by_filename(
-                ['JFRC2_MBON-b1-a.nrrd'], dataset='Aso2014'))
+        df = self.qw.get_images_by_filename(['JFRC2_MBON-b1-a.nrrd'])
+        self.assertTrue(not df.empty)
+
+        df_with_dataset = self.qw.get_images_by_filename(['JFRC2_MBON-b1-a.nrrd'], dataset='Aso2014')
+        self.assertTrue(not df_with_dataset.empty)
 
     def test_get_datasets(self):
-        self.assertGreater(len(self.qw.get_datasets()), 10)
-        self.assertGreater(len(self.qw.get_datasets(summary=True)), 10)
+        result = self.qw.get_datasets(summary=False)
+        self.assertIsInstance(result, list)
+        self.assertGreater(len(result), 10)
+
+        df = self.qw.get_datasets()
+        self.assertTrue(not df.empty)
+        self.assertGreater(len(df), 10)
 
     def test_get_templates(self):
-        self.assertGreater(len(self.qw.get_templates()), 2)
-        self.assertGreater(len(self.qw.get_templates(summary=True)), 2)
+        result = self.qw.get_templates(summary=False)
+        self.assertIsInstance(result, list)
+        self.assertGreater(len(result), 2)
+
+        df = self.qw.get_templates()
+        self.assertTrue(not df.empty)
+        self.assertGreater(len(df), 2)
 
 
 class Neo4jConnectTest(unittest.TestCase):
@@ -74,8 +82,7 @@ class Neo4jConnectTest(unittest.TestCase):
 
     def test_lookup_gen(self):
         lookup = self.nc.get_lookup(limit_by_prefix=['FBbt'])
-        print()
+        self.assertIsNotNone(lookup)  # Added an assertion to validate the lookup
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
