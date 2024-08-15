c00fdd48049e18140b4280a314631b5f1e2211c6
Rob Court <rcourt@ed.ac.uk>
2024-08-15 15:43:10 +0100

    adding v3 con test fail

--- src/vfb_connect/neo/neo4j_tools.py
+++ src/vfb_connect/neo/neo4j_tools.py
@@ -95,7 +95,8 @@ class Neo4jConnect:
             print("Falling back to Neo4j v3 connection")
             self.commit = "/db/data/transaction/commit"
             self.headers = {}
-            self.test_connection()
+            if not self.test_connection():
+                raise Exception("Failed to connect to Neo4j.")
 
     def commit_list(self, statements, return_graphs=False):
         """Commit a list of statements to neo4J DB via REST API.
