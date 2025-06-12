8847f2f808e01ff871d6b2c15c4c4f19bc72ff68
Rob Court <robbie.court@gmail.com>
2025-06-12 14:31:11 +0100

    Add initial project structure and versioning for vfb_connect
    - Created pyproject.toml for project metadata and dependencies.
    - Added versioning support with setuptools_scm in src/vfb_connect/_version.py.
    - Included initial distribution package vfb_connect-2.3.1.dev1+ga2bf4ae.d20250612.tar.gz.
    - Defined project details such as authors, classifiers, and URLs.

--- setup.py
+++ setup.py
@@ -4,16 +4,11 @@ import glob
 
 here = path.abspath(path.dirname(__file__))
 
-from get_version import get_version
-__version__ = get_version(__file__)
-del get_version
-
 # Get the long description from the README file
 with open(path.join(here, 'README.md')) as f:
     long_description = f.read()
 
 setup(name='vfb_connect',  
-      version=__version__, 
       packages=find_packages(where='src'),
       package_dir={'': 'src'},
       py_modules=[path.splitext(path.basename(path))[0] for path in glob.glob('src/*.py')],
@@ -24,14 +19,7 @@ setup(name='vfb_connect',
       author='Robert Court', 
       url='https://github.com/VirtualFlyBrain/VFB_connect',
       author_email='rcourt@ed.ac.uk',  
-      install_requires=['requests', 'pandas', 'jsonpath_rw', 'pysolr', 'navis', 'numpy>=1.26.4, <2.0.0', 'seaborn>0.13', 'colormath', 'tqdm',],
       data_files=[('json_schema', ['src/vfb_connect/resources/VFB_TermInfo_queries.json','src/vfb_connect/resources/VFB_results_single_input.json','src/vfb_connect/resources/VFB_results_multi_input.json'])],
-      classifiers=[  
-          'Development Status :: 5 - Production/Stable',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-          'Programming Language :: Python :: 3',
-       ],
       project_urls={
           'Bug Reports': 'https://github.com/VirtualFlyBrain/VFB_connect/issues',
           'Source': 'https://github.com/VirtualFlyBrain/VFB_connect',
